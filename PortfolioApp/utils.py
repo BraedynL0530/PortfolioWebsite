952bb9fc8b9089803ef380b485affd0ca335ea0d
Braedyn L <poohdie10@gmail.com>
2025-12-16 21:44:23 -0500

    Replace React's StrictMode and createRoot with ReactDOM updates, remove unused components, add react-router-dom dependency, and adjust summarizer_model path in Django utils.

--- PortfolioApp/utils.py
+++ PortfolioApp/utils.py
@@ -4,5 +4,5 @@ from pathlib import Path
 
 def summarizeReadme(data):
     BASE_DIR = Path(__file__).resolve().parent.parent
-    MODEL_PATH = os.path.join(BASE_DIR, 'models', 'summarizer_model.joblib')
+    MODEL_PATH = os.path.join(BASE_DIR, 'ml', 'summarizer_model.joblib')
     return None
